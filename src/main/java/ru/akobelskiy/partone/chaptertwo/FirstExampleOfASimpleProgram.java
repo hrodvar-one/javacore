ea84de05caefea869dd6469025190bccb9192675
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2023-11-21 20:51:55 +0600

    2. Первый пример простой программы

---
+++ src/main/java/ru/akobelskiy/partone/chaptertwo/FirstExampleOfASimpleProgram.java
@@ -0,0 +1,7 @@
+package ru.akobelskiy.partone.chaptertwo;
+
+public class FirstExampleOfASimpleProgram {
+    public static void main(String[] args) {
+        System.out.println("Простая программа на Java");
+    }
+}

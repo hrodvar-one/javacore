f02e4aafb6c944c454cb5bfb2ef5df7beb38a3ba
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2023-11-30 12:20:15 +0600

    Цикл for

---
+++ src/main/java/ru/akobelskiy/partone/chapterfive/ForTick.java
@@ -0,0 +1,10 @@
+package ru.akobelskiy.partone.chapterfive;
+
+public class ForTick {
+    public static void main(String[] args) {
+        int n;
+        for (n = 10; n > 0; n--) {
+            System.out.println("Импульс номер " + n);
+        }
+    }
+}

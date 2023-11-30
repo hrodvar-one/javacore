b57556b06ecbbe48c500c431ff9ac627c19f3457
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2023-11-30 07:46:57 +0600

    Одномерные массивы

---
+++ src/main/java/ru/akobelskiy/partone/chapterthree/AutoArray.java
@@ -0,0 +1,8 @@
+package ru.akobelskiy.partone.chapterthree;
+
+public class AutoArray {
+    public static void main(String[] args) {
+        int[] monthDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.println("В апреле " + monthDays[3] + " дней.");
+    }
+}

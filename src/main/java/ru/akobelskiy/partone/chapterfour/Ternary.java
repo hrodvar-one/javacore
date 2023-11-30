856fe4adc79d6fa620593bac14f361531750d30f
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2023-11-30 10:00:50 +0600

    Операция ?

---
+++ src/main/java/ru/akobelskiy/partone/chapterfour/Ternary.java
@@ -0,0 +1,16 @@
+package ru.akobelskiy.partone.chapterfour;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int i;
+        int k;
+        i = 10;
+        k = i < 0 ? -i : i;
+        System.out.print("Абсолютная величина ");
+        System.out.println(i + " равна " + k);
+        i = -10;
+        k = i < 0 ? -i : i;
+        System.out.print("Абсолютная величина ");
+        System.out.println(i + " равна " + k);
+    }
+}

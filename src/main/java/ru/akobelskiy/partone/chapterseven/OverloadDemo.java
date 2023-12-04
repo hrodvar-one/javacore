1f5b2e5d8d995f8bb87b2a3e63b6097f732e214a
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2023-12-04 07:05:12 +0600

    Перегрузка методов

---
+++ src/main/java/ru/akobelskiy/partone/chapterseven/OverloadDemo.java
@@ -0,0 +1,20 @@
+package ru.akobelskiy.partone.chapterseven;
+
+public class OverloadDemo {
+    void test() {
+        System.out.println("Параметры отстутствуют");
+    }
+
+    void test(int a) {
+        System.out.println("a: " + a);
+    }
+
+    void test(int a, int b) {
+        System.out.println("a и b: " + a + " " + b);
+    }
+
+    double test(double a) {
+        System.out.println("double a: " + a);
+        return a * a;
+    }
+}

6c7bb088c27dc05007773b8c3acc79ef83628354
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2023-12-04 08:45:34 +0600

    Ключевое слово static

---
+++ src/main/java/ru/akobelskiy/partone/chapterseven/StaticByName.java
@@ -0,0 +1,8 @@
+package ru.akobelskiy.partone.chapterseven;
+
+public class StaticByName {
+    public static void main(String[] args) {
+        StaticDemo.callme();
+        System.out.println("b = " + StaticDemo.b);
+    }
+}

fef896159a90971b6055196638b7fcd056c37d5d
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2023-12-05 12:42:04 +0600

    Импортирование пакетов

---
+++ src/main/java/ru/akobelskiy/partone/chapternine/mypack/TestBalance.java
@@ -0,0 +1,8 @@
+package ru.akobelskiy.partone.chapternine.mypack;
+
+public class TestBalance {
+    public static void main(String[] args) {
+        Balance test = new Balance("J. J. Jaspers", 99.88);
+        test.show();
+    }
+}

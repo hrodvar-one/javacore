c788963d70b313e13d37607d0ebe13f25363d52f
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2023-12-05 07:43:05 +0600

    Динамическая диспетчеризация методов

---
+++ src/main/java/ru/akobelskiy/partone/chaptereight/A2.java
@@ -0,0 +1,7 @@
+package ru.akobelskiy.partone.chaptereight;
+
+public class A2 {
+    void callme() {
+        System.out.println("Внутри метода callme() класса A2");
+    }
+}
---
+++ src/main/java/ru/akobelskiy/partone/chaptereight/Dispatch.java
@@ -0,0 +1,16 @@
+package ru.akobelskiy.partone.chaptereight;
+
+public class Dispatch {
+    public static void main(String[] args) {
+        A2 a2 = new A2();
+        B2 b2 = new B2();
+        C2 c2 = new C2();
+        A2 r;
+        r = a2;
+        r.callme();
+        r = b2;
+        r.callme();
+        r = c2;
+        r.callme();
+    }
+}

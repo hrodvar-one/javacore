d8ba5f73d40a57e5b0727a026520669320b841fa
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2023-12-06 07:15:57 +0600

    Закрытые методы интерфейса

--- src/main/java/ru/akobelskiy/partone/chapternine/IntStack.java
+++ src/main/java/ru/akobelskiy/partone/chapternine/IntStack.java
@@ -5,6 +5,23 @@ public interface IntStack {
 
     int pop();
 
+    default int[] popNElements(int n) {
+        return getElements(n);
+    }
+
+    default int[] skipAndPopNElements(int skip, int n) {
+        getElements(skip);
+        return getElements(n);
+    }
+
+    private int[] getElements(int n) {
+        int[] elements = new int[n];
+        for (int i = 0; i < n; i++) {
+            elements[i] = pop();
+        }
+        return elements;
+    }
+
     default void clear() {
         System.out.println("Метод clear() не реализован.");
     }

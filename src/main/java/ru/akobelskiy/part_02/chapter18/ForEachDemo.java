6a7604f7a50f3d096f6250e14c5bd9a59fff5846
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-09-19 10:33:38 +0600

    Глава 18: Цикл for в стиле for each как альтернатива итераторам

---
+++ src/main/java/ru/akobelskiy/part_02/chapter18/ForEachDemo.java
@@ -0,0 +1,36 @@
+package ru.akobelskiy.part_02.chapter18;
+
+/*
+Применение цикла for в стиле for each для перебора элементов коллекции
+ */
+
+import java.util.ArrayList;
+
+public class ForEachDemo {
+    public static void main(String[] args) {
+        // создать списочный массив для целых чисел
+        ArrayList<Integer> vals = new ArrayList<Integer>();
+
+        // ввести числовые значения в списочный массив
+        vals.add(1);
+        vals.add(2);
+        vals.add(3);
+        vals.add(4);
+        vals.add(5);
+
+        // организовать цикл для вывода числовых значений
+        System.out.print("Исходное содержимое списочного массива vals: ");
+        for (int v : vals) {
+            System.out.print(v + " ");
+        }
+        System.out.println();
+
+        // суммировать числовые значения в цикле for
+        int sum = 0;
+        for (int v : vals) {
+            sum += v;
+        }
+
+        System.out.println("Сумма числовых значений: " + sum);
+    }
+}
